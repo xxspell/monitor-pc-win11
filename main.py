ba0870f35e431e15dce364aa9ac3ce0a17395534
hachimaru <74972395+xxspell@users.noreply.github.com>
2024-06-26 21:41:17 +0500

    feat: add toggleable second menu with IP and crypto prices
    - Added functionality to toggle between two menus by clicking on the icon.
    - Display system information in the default view.
    - Display IP address and cryptocurrency prices (BTC, ETH, SOL, TON) in the second view.
    - Implemented alternating display of SOL and TON in the second view for more compact information presentation.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import asyncio
+import psutil
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from asyncqt import QEventLoop, asyncSlot
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QPainter, QColor, QFont
-from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtCore import Qt, QRect, QTimer, QEvent
 from PyQt5.QtSvg import QSvgWidget
-import psutil
-import time
-import threading
 import signal
 import sys
-import subprocess
+import requests
+
+# Настройка логирования
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
 
 
 class BaseWidget(QWidget):
@@ -21,30 +26,44 @@ class CustomWidget(BaseWidget):
     def __init__(self):
         super().__init__()
 
-        self.svg_widget = QSvgWidget(
-            "icon.svg", self
-        )  # Замените 'icon.svg' на путь к вашему SVG-изображению
-        self.svg_widget.setGeometry(
-            10, 13, 25, 25
-        )  # Установите размер и позицию изображения
+        self.svg_widget = QSvgWidget("icon.svg", self)
+        self.svg_widget.setGeometry(10, 13, 25, 25)
         self.svg_widget.setStyleSheet("color: white;")
+        self.svg_widget.installEventFilter(self)
 
-        # Создаем метку для отображения информации
         self.info_label = QLabel(self)
         self.info_label.setGeometry(35, 10, 264, 28)
         font = QFont("Calibri", 10)
         self.info_label.setStyleSheet("color: white;")
         self.info_label.setFont(font)
-        # Обновляем информацию при создании виджета
-        self.update_thread = threading.Thread(
-            target=self.update_info_thread, daemon=True
+
+        self.show_system_info = True
+        self.loading = False
+        self.alternate_flag = True
+
+        self.ip_info = "Loading..."
+        self.crypto_info = {"BTC": "...", "ETH": "...", "SOL": "...", "TON": "..."}
+
+        logging.debug("Initializing event loop")
+        loop = asyncio.get_event_loop()
+        logging.debug("Creating initial task for update_initial_info")
+        loop.create_task(self.update_initial_info())
+
+        logging.debug("Setting up timers")
+        self.system_timer = QTimer(self)
+        self.system_timer.timeout.connect(self.update_system_info)
+        self.system_timer.start(1000)  # обновление каждую секунду
+
+        self.additional_timer = QTimer(self)
+        self.additional_timer.timeout.connect(
+            lambda: asyncio.ensure_future(self.update_additional_info())
         )
-        self.update_thread.start()
+        self.additional_timer.start(600000)  # обновление каждые 10 минут
+
+        self.executor = ThreadPoolExecutor(max_workers=5)
 
     def paintEvent(self, event):
         painter = QPainter(self)
-
-        # Отрисовываем фон
         painter.setRenderHint(QPainter.Antialiasing, False)
         painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
         painter.setPen(QColor(255, 255, 255, 0))
@@ -56,72 +75,144 @@ class CustomWidget(BaseWidget):
         radius = 10
         painter.drawRoundedRect(QRect(x, y, width, height), radius, radius)
 
-        # # Рисуем иконку слева от текста
-        # icon_path = os.path.join(os.path.dirname(__file__), 'icon.png')
-        # icon_pixmap = QPixmap(icon_path)
-        # icon_rect = QRect(10, 10, 16, 16)  # Размер и позиция иконки
-        # painter.drawPixmap(icon_rect, icon_pixmap)
+    def eventFilter(self, source, event):
+        if source == self.svg_widget and event.type() == QEvent.MouseButtonPress:
+            logging.debug("Icon clicked")
+            self.on_icon_click()
+        return super().eventFilter(source, event)
 
-    def update_info_thread(self):
-        while True:
-            while True:
-                try:
-                    result = subprocess.run(
-                        ["wmic", "cpu", "get", "loadpercentage"],
-                        capture_output=True,
-                        text=True,
-                    )
-                    output_lines = result.stdout.strip().split("\n")
+    async def update_initial_info(self):
+        logging.debug("Starting initial update of additional info")
+        await self.update_additional_info()
+        self.show_system_info = True
+        logging.debug(
+            "Initial update of additional info complete, updating system info"
+        )
+        self.update_system_info()
 
-                    # Пропустить первую строку с заголовком "LoadPercentage"
-                    load_percentage_line = (
-                        output_lines[-1] if len(output_lines) > 1 else "0"
-                    )
-
-                    load_percentage = load_percentage_line.strip()
-                    load_percentage = float(load_percentage)
-                    break
-                except Exception as e:
-                    print(f"Произошла ошибка: {e}")
-
-            # cpu_percent = psutil.cpu_percent(interval=1)
-            # cpu_percent = get_cpu_usage()
+    @asyncSlot()
+    async def update_system_info(self):
+        if self.show_system_info:
+            logging.debug("Updating system info")
+            cpu_percent = psutil.cpu_percent(interval=1)
             ram_info = psutil.virtual_memory()
             disk_info_c = psutil.disk_usage("C:")
             disk_info_d = psutil.disk_usage("D:")
 
             info_text = (
-                f"CPU: {load_percentage}% | "
+                f"CPU: {self.format_percentage(cpu_percent)} | "
                 f"RAM: {self.format_size_ram(ram_info.used)} / {self.format_size_ram(ram_info.total)}\n"
                 f"C: {self.format_size(disk_info_c.used)} / {self.format_size(disk_info_c.total)} | "
                 f"D: {self.format_size(disk_info_d.used)} / {self.format_size(disk_info_d.total)}"
             )
-            # print(info_text)
-
-            # Используем метод self.update() для обновления интерфейса в основном потоке
             self.update_label(info_text)
-            time.sleep(2)
+
+    async def update_additional_info(self):
+        logging.debug("Updating additional info (IP and crypto prices)")
+        loop = asyncio.get_event_loop()
+        ip_info = await loop.run_in_executor(self.executor, self.fetch_ip_info)
+        crypto_info = await loop.run_in_executor(self.executor, self.fetch_crypto_info)
+        self.ip_info = ip_info
+        self.crypto_info = crypto_info
+        if not self.show_system_info:
+            logging.debug("System info not shown, displaying additional info")
+            self.show_additional_info()
 
     def update_label(self, text):
-        # Используем метод self.update() для обновления интерфейса в основном потоке
+        logging.debug(f"Updating label with text: {text}")
         self.info_label.setText(text)
         self.update()
 
+    def format_percentage(self, value):
+        return f"{value:.0f}%"
+
     def format_size(self, size):
-        gb_size = size / (1024**3)  # Перевести в GB
+        gb_size = size / (1024**3)
         return f"{gb_size:.0f} GB"
 
     def format_size_ram(self, size):
-        gb_size = size / (1024**3)  # Перевести в GB
+        gb_size = size / (1024**3)
         return f"{gb_size:.2f} GB"
+
+    def on_icon_click(self):
+        self.show_system_info = not self.show_system_info
+        logging.info(
+            f"Icon click detected. Switching to {'system info' if self.show_system_info else 'additional info'} view."
+        )
+        if self.show_system_info:
+            logging.debug("Starting system info timer")
+            self.system_timer.start(1000)  # обновление каждую секунду
+            self.additional_timer.start(600000)  # обновление каждые 10 минут
+        else:
+            logging.debug(
+                "Stopping system info timer and starting additional info timer"
+            )
+            self.system_timer.stop()
+            self.additional_timer.start(60000)  # обновление каждую минуту
+            self.show_additional_info()
+            asyncio.ensure_future(self.update_additional_info())
+
+    def show_additional_info(self):
+        logging.debug("Displaying additional info")
+        ip_info = self.ip_info
+        crypto_info = self.crypto_info
+        if self.alternate_flag:
+            info_text = (
+                f"IP: {ip_info} | BTC: ${crypto_info.get('BTC', 'N/A')}\n"
+                f"ETH: ${crypto_info.get('ETH', 'N/A')} | SOL: ${crypto_info.get('SOL', 'N/A')}"
+            )
+        else:
+            info_text = (
+                f"IP: {ip_info} | BTC: ${crypto_info.get('BTC', 'N/A')}\n"
+                f"ETH: ${crypto_info.get('ETH', 'N/A')} | TON: ${crypto_info.get('TON', 'N/A')}"
+            )
+        self.alternate_flag = not self.alternate_flag
+        self.update_label(info_text)
+
+    def fetch_ip_info(self):
+        try:
+            logging.debug("Fetching IP info")
+            response = requests.get(
+                "https://proxy-checker-new.dolphin-anty-mirror.org/ip-info"
+            )
+            response.raise_for_status()
+            ip_data = response.json()
+            logging.info("Fetched IP info successfully.")
+            return ip_data.get("ip", "Unknown")
+        except Exception as e:
+            logging.error(f"Error fetching IP info: {e}")
+            return "Error"
+
+    def fetch_crypto_info(self):
+        try:
+            logging.debug("Fetching crypto prices")
+            response = requests.get(
+                "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,the-open-network&vs_currencies=usd"
+            )
+            response.raise_for_status()
+            prices = response.json()
+            logging.info("Fetched crypto prices successfully.")
+            return {
+                "BTC": prices.get("bitcoin", {}).get("usd", "N/A"),
+                "ETH": prices.get("ethereum", {}).get("usd", "N/A"),
+                "SOL": prices.get("solana", {}).get("usd", "N/A"),
+                "TON": prices.get("the-open-network", {}).get("usd", "N/A"),
+            }
+        except Exception as e:
+            logging.error(f"Error fetching crypto prices: {e}")
+            return {}
 
 
 def handle_ctrl_c(signum, frame):
+    logging.info("Program interrupted. Exiting...")
     sys.exit()
 
 
 if __name__ == "__main__":
     app = QApplication([])
+
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
 
     desktop = QApplication.desktop()
     desktop_rect = desktop.availableGeometry()
@@ -137,7 +228,6 @@ if __name__ == "__main__":
         Qt.FramelessWindowHint
         | Qt.WindowStaysOnTopHint
         | Qt.X11BypassWindowManagerHint
-        # | Qt.SplashScreen
         | Qt.WindowDoesNotAcceptFocus
         | Qt.Tool
     )
@@ -146,4 +236,6 @@ if __name__ == "__main__":
 
     window.show()
 
-    app.exec()
+    logging.info("Application started.")
+    with loop:
+        loop.run_forever()
